399c5b9917dd862bcfa6556dbf906561134abf1e
PhcBest <phcbest2017@outlook.com>
2023-10-17 00:00:16 +0800

    添加BaseActivity

--- app/src/main/java/com/gowow/phcmediacodecdemo/MainActivity.kt
+++ app/src/main/java/com/gowow/phcmediacodecdemo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.gowow.phcmediacodecdemo
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.gowow.phcmediacodecdemo.base.BaseActivity
+import com.gowow.phcmediacodecdemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
     }
 }
---
+++ app/src/main/java/com/gowow/phcmediacodecdemo/base/BaseActivity.kt
@@ -0,0 +1,17 @@
+package com.gowow.phcmediacodecdemo.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B : ViewBinding>(val bindingFactory: (LayoutInflater) -> B) :
+    AppCompatActivity() {
+    private lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+    }
+}
