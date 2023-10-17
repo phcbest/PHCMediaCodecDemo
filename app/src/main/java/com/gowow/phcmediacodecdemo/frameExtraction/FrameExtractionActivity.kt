992cb09cee3a64d57d07cbe707366a049fa7749f
PhcBest <phcbest2017@outlook.com>
2023-10-18 00:01:48 +0800

    添加视频选择器

--- app/src/main/java/com/gowow/phcmediacodecdemo/frameExtraction/FrameExtractionActivity.kt
+++ app/src/main/java/com/gowow/phcmediacodecdemo/frameExtraction/FrameExtractionActivity.kt
@@ -2,13 +2,32 @@ package com.gowow.phcmediacodecdemo.frameExtraction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.gowow.phcmediacodecdemo.R
 import com.gowow.phcmediacodecdemo.base.BaseActivity
 import com.gowow.phcmediacodecdemo.databinding.ActivityFrameExtractionBinding
 
 class FrameExtractionActivity :
+
     BaseActivity<ActivityFrameExtractionBinding>(ActivityFrameExtractionBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        selectVideo()
+    }
+
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
+    private fun selectVideo() {
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
+
     }
 }
