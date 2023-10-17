80ea318a5e552728cae953fec25f98630718d188
PhcBest <phcbest2017@outlook.com>
2023-10-18 00:11:49 +0800

    添加视频播放器

--- app/src/main/java/com/gowow/phcmediacodecdemo/frameExtraction/FrameExtractionActivity.kt
+++ app/src/main/java/com/gowow/phcmediacodecdemo/frameExtraction/FrameExtractionActivity.kt
@@ -1,11 +1,11 @@
 package com.gowow.phcmediacodecdemo.frameExtraction
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import com.gowow.phcmediacodecdemo.R
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import com.gowow.phcmediacodecdemo.base.BaseActivity
 import com.gowow.phcmediacodecdemo.databinding.ActivityFrameExtractionBinding
 
@@ -21,6 +21,12 @@ class FrameExtractionActivity :
         registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
             if (uri != null) {
                 Log.d("PhotoPicker", "Selected URI: $uri")
+                binding.player.useController = false
+                binding.player.player = ExoPlayer.Builder(this).build().also {
+                    it.setMediaItem(MediaItem.fromUri(uri))
+                    it.prepare()
+                    it.play()
+                }
             } else {
                 Log.d("PhotoPicker", "No media selected")
             }
