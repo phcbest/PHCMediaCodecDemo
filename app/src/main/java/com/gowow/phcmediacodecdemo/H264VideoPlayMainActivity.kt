5309867d174bf41766dc04ca6514da182840977a
PhcBest <phcbest2017@outlook.com>
2023-10-24 23:18:38 +0800

    更新代码，解决无法初始化SurfaceView问题

--- app/src/main/java/com/gowow/phcmediacodecdemo/H264VideoPlayMainActivity.kt
+++ app/src/main/java/com/gowow/phcmediacodecdemo/H264VideoPlayMainActivity.kt
@@ -3,8 +3,12 @@ package com.gowow.phcmediacodecdemo
 import android.media.MediaCodec
 import android.media.MediaFormat
 import android.os.Bundle
+import android.view.SurfaceHolder
 import com.gowow.phcmediacodecdemo.base.BaseActivity
 import com.gowow.phcmediacodecdemo.databinding.ActivityH264VideoPlayMainBinding
+import java.io.DataInputStream
+import java.io.FileInputStream
+import java.io.IOException
 
 /**
  * [参考文章](https://juejin.cn/post/7213733567606652985)
@@ -17,6 +21,8 @@ class H264VideoPlayMainActivity :
     }
 
     private var mediaCodec: MediaCodec? = null
+
+
     private fun initMediaCodec() {
         //解码器按照类型设置为aac
         mediaCodec = MediaCodec.createDecoderByType("video/avc")
@@ -25,6 +31,42 @@ class H264VideoPlayMainActivity :
         //视频解码预期帧速率
         mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30)
         //绑定解码Surface
-//        mediaCodec?.configure()
+        binding.surfaceVideo.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                mediaCodec?.configure(mediaFormat, binding.surfaceVideo.holder.surface, null, 0)
+                decodePlay()
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder, format: Int, width: Int, height: Int
+            ) {
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+            }
+
+        })
+    }
+
+    private fun decodePlay() {
+        mediaCodec?.start()
+        Thread(MyRun()).start()
+    }
+
+    class MyRun : Runnable {
+        override fun run() {
+            try {
+                //io流方式读取h264
+                var bytes: ByteArray? = null
+//                bytes = getBytes(videoPath)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+//        private fun getBytes(videoPath: Any): ByteArray? {
+//            DataInputStream(FileInputStream(videoPath))
+//        }
+
     }
 }
