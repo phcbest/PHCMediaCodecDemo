336081f6cc972bf988580e208cc4261cb1e93cab
PhcBest <phcbest2017@outlook.com>
2023-10-25 23:21:16 +0800

    完成demo,但是为什么没法播放出来？

--- app/src/main/java/com/gowow/phcmediacodecdemo/H264VideoPlayMainActivity.kt
+++ app/src/main/java/com/gowow/phcmediacodecdemo/H264VideoPlayMainActivity.kt
@@ -81,10 +81,53 @@ class H264VideoPlayMainActivity :
                 Log.e(TAG, "bytes size " + bytes.size)
                 val inputBuffers = mediaCodec!!.inputBuffers
                 //开始位置
-                val startIndex = 0
+                var startIndex = 0
                 //h264总字节数
                 val totalSize: Int = bytes.size
-
+                while (true) {
+                    // 检查是否符合解码条件
+                    if (totalSize == 0 || startIndex >= totalSize) {
+                        break
+                    }
+                    // 查找下一帧的起始位置
+                    val nextFrameStart = findByFrame(bytes, startIndex + 1, totalSize)
+                    if (nextFrameStart == -1) break
+                    val info = MediaCodec.BufferInfo()
+                    // 获取可用的输入缓冲区索引
+                    val inIndex = mediaCodec?.dequeueInputBuffer(10000)
+                    if ((inIndex ?: -1) >= 0) {
+                        // 根据索引获取可用的缓冲区
+                        val byteBuffer = inIndex?.let { inputBuffers[it] }
+                        // 清空缓冲区
+                        byteBuffer?.clear()
+                        // 将数据填充到缓冲区
+                        byteBuffer?.put(bytes, startIndex, nextFrameStart - startIndex)
+                        // 填充数据后通知MediaCodec查询该索引的缓冲区
+                        mediaCodec?.queueInputBuffer(
+                            inIndex ?: -1, 0, nextFrameStart - startIndex, 0, 0
+                        )
+                        // 为下一帧做准备，下一帧的起始位置就是前一帧的结束位置
+                        startIndex = nextFrameStart
+                    } else {
+                        // 等待可用的缓冲区
+                        continue
+                    }
+                    // 从MediaCodec的输出缓冲区获取索引
+                    val outIndex = mediaCodec?.dequeueOutputBuffer(info, 10000)
+                    Log.e(TAG, "outIndex $outIndex")
+                    if ((outIndex ?: -1) >= 0) {
+                        try {
+                            // 暂时以线程休眠的方式减缓播放速度
+                            Thread.sleep(33)
+                        } catch (e: InterruptedException) {
+                            e.printStackTrace()
+                        }
+                        // 如果Surface已绑定，则将数据渲染到Surface并释放缓冲区
+                        mediaCodec?.releaseOutputBuffer(outIndex ?: -1, true)
+                    } else {
+                        Log.e(TAG, "没有解码成功")
+                    }
+                }
 
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -103,5 +146,16 @@ class H264VideoPlayMainActivity :
             return bos.toByteArray()
         }
 
+    }
+
+    // 读取一帧数据
+    private fun findByFrame(bytes: ByteArray, start: Int, totalSize: Int): Int {
+        for (i in start until totalSize - 4) {
+            // 根据0x00000001作为分隔符来读取实际数据
+            if (bytes[i] == 0x00.toByte() && bytes[i + 1] == 0x00.toByte() && bytes[i + 2] == 0x00.toByte() && bytes[i + 3] == 0x01.toByte()) {
+                return i
+            }
+        }
+        return -1
     }
 }
